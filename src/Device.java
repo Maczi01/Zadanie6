ee313d5bfed63bf37416da9ee38c9e2be9fc10a2
Maczi01 <system11>
2019-01-20 16:09:58 +0100

    Change in constructor, add private to fields

--- src/Device.java
+++ src/Device.java
@@ -1,9 +1,9 @@
 public class Device {
 
-    String code;
-    String producer;
-    String type;
-    double price;
+    private String code;
+    private String producer;
+    private String type;
+    private double price;
 
     public Device(String code, String producer, String type) {
         this.code = code;
@@ -12,9 +12,7 @@ public class Device {
     }
 
     public Device(String code, String producer, String type, double price) {
-        this.code = code;
-        this.producer = producer;
-        this.type = type;
+        this(code, producer, type);
         this.price = price;
     }
 
